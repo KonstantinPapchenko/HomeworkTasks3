9fe481d1d2115535833c2046cd560c853efbcdef
KonstantinPapchenko <106111694+KonstantinPapchenko@users.noreply.github.com>
2022-12-04 20:34:05 +0300

    added Homework3

---
+++ Seminar3/Homework3/Task19/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задание переписано с разбора на 4м занятии
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());//проверка на ввод числа
+
+while(n < 10000 || n > 99999)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int n1 = n / 10000; 
+int n2 = (n / 1000) % 10;
+int n4 = (n % 100) / 10;
+int n5 = n % 10;
+   
+
+if(n1 == n5 && n2 == n4)
+    Console.WriteLine("yes");
+else 
+    Console.WriteLine("no");
+
+
+
+ 
+/*int number, remind, sum = 0, temp;   
+Console.Write("Введите число: ");  
+number = Convert.ToInt32(Console.ReadLine());  
+temp = number;  
+while (number > 0)  
+{  
+    remind = number % 10;  
+    number = number / 10;  
+    sum = sum * 10 + remind;  
+}  Console.WriteLine("n The Reversed Number of the Original Number  is: {0} n", sum);  
+if (temp == sum)  
+{  
+    Console.WriteLine("n Your Number is a Palindrome nn");  
+}  
+else  
+{  
+    Console.WriteLine("n Your Number is not a Palindrome nn");  
+}  
+Console.ReadLine();  
+*/
---
+++ Seminar3/Homework3/Task23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Ведите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=n; i++)
+     Console.WriteLine($"{i*i*i}");
+
